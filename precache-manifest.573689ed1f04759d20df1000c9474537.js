3e67b831d14e7de11778aa41699ef076add9bfae
Kushagra Anand <kushrokz95@gmail.com>
2020-05-24 22:43:27 +0530

    Updates

---
+++ precache-manifest.573689ed1f04759d20df1000c9474537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66bed522908b42d10c165f454738809",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "2649bae0d5d73b82db46",
+    "url": "/Covid-19/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "79c86e70463910e09d0e",
+    "url": "/Covid-19/static/css/main.3203dd94.chunk.css"
+  },
+  {
+    "revision": "2649bae0d5d73b82db46",
+    "url": "/Covid-19/static/js/2.29649383.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/Covid-19/static/js/2.29649383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c86e70463910e09d0e",
+    "url": "/Covid-19/static/js/main.05678c30.chunk.js"
+  },
+  {
+    "revision": "844a34b7fb17e1ad6b4a",
+    "url": "/Covid-19/static/js/runtime-main.67642129.js"
+  }
+]);
